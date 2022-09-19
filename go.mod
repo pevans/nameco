e892afbb75e53c6b7f0e260a034149b0cc23dc99
Peter Evans <peter@modul8r.com>
2022-09-18 21:35:56 -0500

    Add main function

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pevans/nameco
+
+go 1.19
+
+require github.com/moby/moby v20.10.18+incompatible // indirect
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/moby/moby/pkg/namesgenerator"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println(namesgenerator.GetRandomName(0))
+}
