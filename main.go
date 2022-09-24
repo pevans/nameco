b3c0fc6e885986c76303d0aef7e94889436998dc
Peter Evans <peter@modul8r.com>
2022-09-24 12:41:37 -0500

    Print a name or a word

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/moby/moby/pkg/namesgenerator"
@@ -11,5 +13,27 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	fmt.Println(namesgenerator.GetRandomName(0))
+	command := "name"
+	if len(os.Args) > 1 {
+		command = os.Args[1]
+	}
+
+	var out string
+
+	switch command {
+	case "name":
+		out = namesgenerator.GetRandomName(0)
+	case "word":
+		parts := strings.Split(namesgenerator.GetRandomName(0), "_")
+		if len(parts) > 0 {
+			out = parts[0]
+		}
+	}
+
+	if out == "" {
+		fmt.Println("there's no name to print... 🤔")
+		os.Exit(1)
+	}
+
+	fmt.Println(out)
 }
